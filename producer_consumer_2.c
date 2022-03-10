97c58599cd29819e0908b0710b4cd4724a47664a
pinglarin <pinglarin@gmail.com>
2022-03-10 20:17:37 +0700

    producer_wait completed

--- producer_consumer_2.c
+++ producer_consumer_2.c
@@ -17,10 +17,10 @@ double secs = 0;
 
 int buffer[BUFFER_SIZE];
 int in = 0, out = 0;
-
+int nHead = 0;
 
 static volatile int keepRunning = 1;
-int nprocess, ndevice, nrequest, mintime, maxtime, currRequest = 0, finRequest = 0;
+int nprocess, ndevice, nrequest, mintime, maxtime, currRequest = 0, finRequest = 0, producerType;
 
 void intHandler(int dummy) {
     keepRunning = 0;
@@ -43,14 +43,19 @@ void print_list(node_t *head, char c);
 void sleep_ms(int milliseconds);
 int random_int(int min, int max);
 
-void *producer(void * id_ptr) {
+
+void *producer_wait(void * id_ptr) {
     int ID = *((int *) id_ptr);
     static int nextProduced = 0;
     struct timeval start;
+    
     while (currRequest < nrequest) {
-        (void) sem_wait(empty);
+        // printf("%d %d %d\n", ID, currRequest, nrequest);
+        currRequest++;
+        sem_wait(empty);
         // (void) sem_wait(mutex);
         pthread_mutex_lock(&mutex);
+
 
        /* Check to see if Overwriting unread slot */
         if (buffer[in] != -1) {
@@ -59,7 +64,7 @@ void *producer(void * id_ptr) {
         }
 
         
-
+        
         sleep_ms(random_int(100, 500));
         gettimeofday(&start, NULL);
         
@@ -67,19 +72,117 @@ void *producer(void * id_ptr) {
         
         enqueue(&head, nextProduced);
         enqueue(&timeArrive, start.tv_sec);
-
+        nHead++;
+        // printf("*%d %d %d\n", ID, currRequest, nrequest);
         /* Looks like we are OK */
         buffer[in] = nextProduced;
+        printf("queue: %d\n", nHead);
         printf("Process %d has issued a request %d at slot %d, start: %ld\n", ID, nextProduced, in, start.tv_sec);
         in = (in + 1) % BUFFER_SIZE;
-        currRequest++;
+        
         // printf("incremented in!\n");
 
         pthread_mutex_unlock(&mutex);
         
-
         // (void) sem_post(mutex);
-        (void) sem_post(full);
+        sem_post(full);
+    }
+
+    return NULL;
+}
+
+void *producer_drop(void * id_ptr) {
+    int ID = *((int *) id_ptr);
+    static int nextProduced = 0;
+    struct timeval start;
+    
+    while (currRequest < nrequest) {
+        // printf("%d %d %d\n", ID, currRequest, nrequest);
+        currRequest++;
+        sem_wait(empty);
+        // (void) sem_wait(mutex);
+        pthread_mutex_lock(&mutex);
+
+
+       /* Check to see if Overwriting unread slot */
+        if (buffer[in] != -1) {
+            fprintf(stderr, "Synchronization Error: Producer %d Just overwrote %d from Slot %d\n", ID, buffer[in], in);
+            exit(1);
+        }
+
+        
+        
+        sleep_ms(random_int(100, 500));
+        gettimeofday(&start, NULL);
+        
+        nextProduced++; // Producing Integers
+        
+        enqueue(&head, nextProduced);
+        enqueue(&timeArrive, start.tv_sec);
+        nHead++;
+        // printf("*%d %d %d\n", ID, currRequest, nrequest);
+        /* Looks like we are OK */
+        buffer[in] = nextProduced;
+        printf("queue: %d\n", nHead);
+        printf("Process %d has issued a request %d at slot %d, start: %ld\n", ID, nextProduced, in, start.tv_sec);
+        in = (in + 1) % BUFFER_SIZE;
+        
+        // printf("incremented in!\n");
+
+        pthread_mutex_unlock(&mutex);
+        
+        // (void) sem_post(mutex);
+        sem_post(full);
+    }
+
+    return NULL;
+}
+
+void *producer_replace(void * id_ptr) {
+    int ID = *((int *) id_ptr);
+    static int nextProduced = 0;
+    struct timeval start;
+    
+    while (currRequest < nrequest) {
+        // printf("%d %d %d\n", ID, currRequest, nrequest);
+        currRequest++;
+        sem_wait(empty);
+        // (void) sem_wait(mutex);
+        pthread_mutex_lock(&mutex);
+
+
+       /* Check to see if Overwriting unread slot */
+        if (buffer[in] != -1) {
+            fprintf(stderr, "Synchronization Error: Producer %d Just overwrote %d from Slot %d\n", ID, buffer[in], in);
+            exit(1);
+        }
+
+        
+        
+        sleep_ms(random_int(100, 500));
+        gettimeofday(&start, NULL);
+        
+        nextProduced++; // Producing Integers'
+
+        // dequeue(&head);
+        // enqueue(&head, nextProduced)
+        
+        enqueue(&head, nextProduced);
+        enqueue(&timeArrive, start.tv_sec);
+        nHead++;
+        // printf("*%d %d %d\n", ID, currRequest, nrequest);
+        /* Looks like we are OK */
+        buffer[in] = nextProduced;
+        printf("queue: %d\n", nHead);
+        printf("Process %d has issued a request %d at slot %d, start: %ld\n", ID, nextProduced, in, start.tv_sec);
+        in = (in + 1) % BUFFER_SIZE;
+        
+        // printf("incremented in!\n");
+
+        pthread_mutex_unlock(&mutex);
+        
+        // (void) sem_post(mutex);
+        sem_post(full);
     }
 
     return NULL;
@@ -89,11 +192,18 @@ void *consumer (void *id_ptr) {
     int ID = *((int *) id_ptr);
     static int nextConsumed = 0;
     struct timeval stop;
+    
     while (finRequest < nrequest) {
-        (void) sem_wait(full);
+        printf("\t%d %d %d %d\n", ID, currRequest, nrequest, buffer[ID]);
+        if (buffer[ID] == 0) {
+            break;
+        }
+        sem_wait(full);
         // (void) sem_wait(mutex);
 
         pthread_mutex_lock(&mutex);
+
+        
 
         gettimeofday(&stop, NULL);
         enqueue(&timeStart, stop.tv_sec);
@@ -109,6 +219,7 @@ void *consumer (void *id_ptr) {
         }
 
         /* We must be OK */
+        // printf("queue: %d\n", nHead);
         printf("\tDevice %d Just finished request %d from slot %d, stop: %ld\n", ID, nextConsumed, out, stop.tv_sec);
         buffer[out] = -1;
         out = (out + 1) % BUFFER_SIZE;
@@ -118,7 +229,7 @@ void *consumer (void *id_ptr) {
         pthread_mutex_unlock(&mutex);
 
         // (void) sem_post(mutex);
-        (void) sem_post(empty);
+        sem_post(empty);
     }
 
     return NULL;
@@ -146,14 +257,12 @@ void sleep_ms(int milliseconds){ // cross-platform sleep function
 
 int main() {
     // printf("A");
-    scanf("%d %d %d %d %d", &nprocess, &ndevice, &nrequest, &mintime, &maxtime);
+    scanf("%d %d %d %d %d %d", &nprocess, &ndevice, &nrequest, &mintime, &maxtime, &producerType);
     float timeWait[nrequest];
     int MAX_THREADS = nprocess + ndevice;
     int ID[MAX_THREADS];
     pthread_t TID[MAX_THREADS];
 
-    struct timespec cStart, cEnd;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &cStart);
     clock_t t;
     t = clock();
 
@@ -171,15 +280,30 @@ int main() {
     for (int i = 0; i < BUFFER_SIZE; i++) {
         buffer[i] = -1;
     }
-
-    for (int i = 0; i < nprocess; i++)
-    {
-        pthread_create(&TID[i], NULL, producer, (void *) &ID[i]);
-        printf("Process ID = %d created!\n", i);
+    if (producerType == 1){
+        for (int i = 0; i < nprocess; i++)
+        {
+            pthread_create(&TID[i], NULL, producer_wait, (void *) &ID[i]);
+            printf("Process ID = %d created!\n", i);
+        }
+    }
+    else if (producerType == 2){
+        for (int i = 0; i < nprocess; i++)
+        {
+            pthread_create(&TID[i], NULL, producer_drop, (void *) &ID[i]);
+            printf("Process ID = %d created!\n", i);
+        }
+    }
+    else if (producerType == 3){
+        for (int i = 0; i < nprocess; i++)
+        {
+            pthread_create(&TID[i], NULL, producer_replace, (void *) &ID[i]);
+            printf("Process ID = %d created!\n", i);
+        }
     }
     // int k = nprocess;
     // printf("A: device: %d", ndevice);
-    for (int i = 0; i < ndevice; i++)
+    for (int i = nprocess; i < MAX_THREADS; i++)
     {
         pthread_create(&TID[i], NULL, consumer, (void *) &ID[i]);
         printf("Device ID = %d created!\n", i);
@@ -200,20 +324,17 @@ int main() {
         k++;
     }
     float avg = sum/nrequest;
-    printf("Average wait time: %f\n", avg);
+    printf("Average wait time: %f seconds\n", avg);
 
 
-    (void) sem_unlink("/empty");
-    (void) sem_unlink("/full");
+    sem_unlink("/empty");
+    sem_unlink("/full");
     // (void) sem_unlink("/mutex");
 
-    // gettimeofday(&totalEnd, NULL);
-    clock_gettime(CLOCK_MONOTONIC_RAW, &cEnd);
-    uint64_t delta_us = (cEnd.tv_sec - cStart.tv_sec) * 1000000 + (cEnd.tv_nsec - cStart.tv_nsec) / 1000;
-    t = clock() - t;
-    // double time_taken = ((double)t)/CLOCKS_PER_SEC;
-    printf("The program took %llu milliseconds to execute", delta_us);
-    // printf("Total elapsed time: %f", time_taken); //totalStart.tv_usec-totalEnd.tv_usec
+    clock_t tEnd = clock();
+    double time_taken = (tEnd-t)/1000.0;//((double)t)/CLOCKS_PER_SEC;
+    // printf("The program took %llu milliseconds to execute", delta_us);
+    printf("Total elapsed time: %f seconds", time_taken); //totalStart.tv_usec-totalEnd.tv_usec
     
 
     // print_list(head, '-');
@@ -231,6 +352,7 @@ void enqueue(node_t **head, int val) {
     new_node->next = *head;
 
     *head = new_node;
+    // nqueue++;
 }
 
 int dequeue(node_t **head) {
@@ -253,6 +375,7 @@ int dequeue(node_t **head) {
     else
         *head = NULL;
 
+    // nqueue--;
     return retval;
 }
 
